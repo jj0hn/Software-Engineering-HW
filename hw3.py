b065e4eabdd155052dfac3673dd3f327a05cfbc5
Joanna John <joanna.john123@gmail.com>
2022-01-27 15:56:48 -0500

    testing

---
+++ hw3.py
@@ -0,0 +1,6 @@
+#testing
+#question 1 write a function that takes a string as input and
+#returns true if the input string has more vowels than consonants
+#returns false if the input string has more consonants than vowels
+#returns none if the input string has an equal number of consonants and vowels
+#testing again
